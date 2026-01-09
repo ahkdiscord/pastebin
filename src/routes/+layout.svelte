9c69952aebffbd9075381e912f204be4a2b9b822
Mika <mika@simplymika.com>
2026-01-09 14:26:42 +0100

    Start project

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+  import "$lib/global.css";
+
+  import favicon from "$lib/assets/favicon.ico";
+
+  const { children } = $props();
+</script>
+
+<svelte:head>
+  <link rel="icon" href={favicon} />
+</svelte:head>
+
+{@render children()}
