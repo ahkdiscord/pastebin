fa805715ee49faa2849f6bb61d1b8f70fa0471c0
Mika <mika@simplymika.com>
2026-01-09 14:27:30 +0100

    Add very basic editor implementation

---
+++ src/lib/Editor.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+  import { EditorView } from "codemirror";
+  import { onDestroy, onMount } from "svelte";
+
+  interface Props {
+    content: string;
+  }
+
+  const { content = $bindable() }: Props = $props();
+
+  let wrapper: HTMLDivElement;
+  let view: EditorView | undefined;
+
+  onMount(() => {
+    view = new EditorView({
+      parent: wrapper,
+    });
+
+    console.log(view);
+  });
+
+  onDestroy(() => {
+    view?.destroy();
+  });
+</script>
+
+<div id="editor" bind:this={wrapper}></div>
+
+<style>
+  #editor {
+    display: contents;
+  }
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,90 @@
+<script lang="ts">
+  import Editor from "$lib/Editor.svelte";
+
+  const script = $state("this\nis a\ntest");
+
+  function newClicked() {
+    console.log("click");
+  }
+</script>
+
+<form method="POST">
+  <header>
+    <nav>
+      <ul>
+        <li>
+          <button type="button" onclick={newClicked}>new</button>
+        </li>
+      </ul>
+      <a href="/">ahkbin</a>
+      <ul>
+        <li><button type="submit">paste</button></li>
+      </ul>
+    </nav>
+  </header>
+
+  <Editor content={script} />
+</form>
+
+<style>
+  header {
+    position: sticky;
+
+    background-color: var(--black);
+    color: var(--white);
+
+    display: flex;
+    align-items: center;
+    padding: 0.25em 0.5em;
+
+    font-size: 1.5rem;
+  }
+
+  nav {
+    display: flex;
+    align-items: baseline;
+    flex-grow: 1;
+  }
+
+  ul {
+    display: flex;
+    justify-content: center;
+    align-items: baseline;
+    gap: 1em;
+
+    margin: 0;
+    padding: 0;
+    list-style: none;
+    flex-grow: 1;
+  }
+
+  ul:first-child {
+    justify-content: start;
+  }
+
+  ul:last-child {
+    justify-content: end;
+  }
+
+  a {
+    text-decoration: unset;
+    color: unset;
+    display: block;
+  }
+
+  button,
+  a {
+    border: none;
+    border-radius: 0.25em;
+    padding: 0.25em 0.5em;
+    color: inherit;
+    background-color: transparent;
+    font-size: inherit;
+    cursor: pointer;
+  }
+
+  button:hover,
+  a:hover {
+    background-color: var(--slate);
+  }
+</style>
