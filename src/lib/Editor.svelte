d360bdc3cdcc8efa0e24075d889900a6f38d374e
Mika <mika@simplymika.com>
2026-01-09 15:45:08 +0100

    Setup and style editor better

--- src/lib/Editor.svelte
+++ src/lib/Editor.svelte
@@ -1,6 +1,21 @@
 <script lang="ts">
+  import { Compartment, EditorState } from "@codemirror/state";
   import { EditorView } from "codemirror";
+  import {
+    drawSelection,
+    dropCursor,
+    highlightActiveLine,
+    highlightActiveLineGutter,
+    highlightSpecialChars,
+    keymap,
+    lineNumbers,
+    rectangularSelection,
+  } from "@codemirror/view";
   import { onDestroy, onMount } from "svelte";
+  import { defaultKeymap, history } from "@codemirror/commands";
+  import { bracketMatching, foldGutter, indentOnInput } from "@codemirror/language";
+  import { autocompletion, closeBrackets, closeBracketsKeymap } from "@codemirror/autocomplete";
+  import { highlightSelectionMatches } from "@codemirror/search";
 
   interface Props {
     content: string;
@@ -11,9 +26,35 @@
   let wrapper: HTMLDivElement;
   let view: EditorView | undefined;
 
+  const tabSize = new Compartment();
+
+  const state = EditorState.create({
+    extensions: [
+      lineNumbers(),
+      drawSelection(),
+      keymap.of([...defaultKeymap, ...closeBracketsKeymap]),
+      history(),
+      tabSize.of(EditorState.tabSize.of(4)),
+      foldGutter(),
+      dropCursor(),
+      EditorState.allowMultipleSelections.of(true),
+      indentOnInput(),
+      bracketMatching(),
+      closeBrackets(),
+      autocompletion(),
+      rectangularSelection(),
+      highlightActiveLine(),
+      highlightActiveLineGutter(),
+      highlightSelectionMatches(),
+      highlightSpecialChars(),
+      EditorView.theme({}, { dark: true }),
+    ],
+  });
+
   onMount(() => {
     view = new EditorView({
       parent: wrapper,
+      state,
     });
 
     console.log(view);
