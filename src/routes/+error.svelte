5e2f2de1d7e0345d98bec1cbc822fb7a6df927c7
Mika <mika@simplymika.com>
2026-01-16 17:58:50 +0100

    Add error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+  import { page } from "$app/state";
+</script>
+
+<h1>{page.status}</h1>
+<h2>{page.error!.message}</h2>
+
+<style>
+  h1,
+  h2 {
+    line-height: 1;
+    margin: 0;
+    text-align: center;
+  }
+  h1 {
+    font-size: 5rem;
+  }
+  h2 {
+    font-size: 1rem;
+  }
+</style>
