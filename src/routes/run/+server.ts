f0a90fe3227c2b16bea871a2bb724697f5c40cf0
Mika <mika@simplymika.com>
2026-02-03 23:45:42 +0100

    Revert "Disallow POST /run from anywhere but the actual sveltekit app"
    This reverts commit b5fc4dbd51ec2a4c54cc800873e9198192727f42.

--- src/routes/run/+server.ts
+++ src/routes/run/+server.ts
@@ -1,16 +1,14 @@
-import { runScript } from "$lib/server/running.js";
-import type { Version } from "$lib/types";
-import { error, json } from "@sveltejs/kit";
+import { runScript } from '$lib/server/running.js';
+import type { Version } from '$lib/types';
+import { json } from '@sveltejs/kit';
 
 interface RunRequest {
   version: Version;
   script: string;
 }
 
-export async function POST({ request, isSubRequest }) {
-  if (!isSubRequest) error(404);
+export async function POST({ request }) {
+    const data: RunRequest = await request.json();
 
-  const data: RunRequest = await request.json();
-
-  return json(await runScript(data.script, data.version));
-}
+    return json(await runScript(data.script, data.version));
+  }
