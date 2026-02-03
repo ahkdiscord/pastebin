b5fc4dbd51ec2a4c54cc800873e9198192727f42
Mika <mika@simplymika.com>
2026-02-03 22:01:33 +0100

    Disallow POST /run from anywhere but the actual sveltekit app

--- src/routes/run/+server.ts
+++ src/routes/run/+server.ts
@@ -1,14 +1,16 @@
-import { runScript } from '$lib/server/running.js';
-import type { Version } from '$lib/types';
-import { json } from '@sveltejs/kit';
+import { runScript } from "$lib/server/running.js";
+import type { Version } from "$lib/types";
+import { error, json } from "@sveltejs/kit";
 
 interface RunRequest {
   version: Version;
   script: string;
 }
 
-export async function POST({ request }) {
-    const data: RunRequest = await request.json();
+export async function POST({ request, isSubRequest }) {
+  if (!isSubRequest) error(404);
 
-    return json(await runScript(data.script, data.version));
-  }
+  const data: RunRequest = await request.json();
+
+  return json(await runScript(data.script, data.version));
+}
